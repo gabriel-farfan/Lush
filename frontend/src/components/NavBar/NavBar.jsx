c105182208cd08583dc0f4bf06e9dd3b58d3d11f
Sergio Robledo <seergiorobledo@gmail.com>
2022-04-04 04:49:21 -0300

    4/4 04:47 front: install / create files

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+function NavBar() {
+  return (
+    <div>
+        <Button variant="contained">Holi</Button>
+    </div>
+  )
+}
+
+export default NavBar
