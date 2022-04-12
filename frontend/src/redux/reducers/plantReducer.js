31e1fa558c8152e18b843b8f0712dd73d2098132
Chantal <chantal.pilia01@gmail.com>
2022-04-12 01:33:14 -0500

    update 12.04 01:32 (filtros)

--- frontend/src/redux/reducers/plantReducer.js
+++ frontend/src/redux/reducers/plantReducer.js
@@ -1,7 +1,22 @@
 const initialState = {
   allPlants: [],
   plants: [],
-  filter: {},
+  filter: {
+    careRatio: [0, 100],
+    lightRatio: [0, 100],
+    waterRatio: [0, 100],
+    minPrice: '',
+    maxPrice: '',
+    size: new Set(['small', 'medium', 'large']),
+    room: new Set([
+      'office',
+      'kitchen',
+      'bathroom',
+      'living room',
+      'bedroom',
+      'garden',
+    ])
+  },
   loaded: false,
   plant: {},
 }
@@ -9,13 +24,26 @@ const initialState = {
 const plantReducer = (state = initialState, action) => {
   let filter = state.filter;
   const doFilter = plant => {
-    if (('careRatio' in filter) && (plant.careRatio < filter.careRatio[0] || plant.careRatio > filter.careRatio[1])) {
+    console.log(filter)
+    if (plant.careRatio * 100 < filter.careRatio[0] || plant.careRatio * 100 > filter.careRatio[1]) {
       return false;
     }
-    if (('lightRatio' in filter) && (plant.lightRatio < filter.lightRatio[0] || plant.lightRatio > filter.lightRatio[1])) {
+    if (plant.lightRatio * 100 < filter.lightRatio[0] || plant.lightRatio * 100 > filter.lightRatio[1]) {
       return false;
     }
-    if (('waterRatio' in filter) && (plant.waterRatio < filter.waterRatio[0] || plant.waterRatio > filter.waterRatio[1])) {
+    if (plant.waterRatio * 100 < filter.waterRatio[0] || plant.waterRatio * 100 > filter.waterRatio[1]) {
+      return false;
+    }
+    if (filter.minPrice !== '' && plant.price < filter.minPrice) {
+      return false;
+    }
+    if (filter.maxPrice !== '' && plant.price > filter.maxPrice) {
+      return false;
+    }
+    if (!filter.size.has(plant.size)) {
+      return false;
+    }
+    if (!filter.room.has(plant.room)) {
       return false;
     }
     return true;
