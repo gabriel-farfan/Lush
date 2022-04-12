c3fb8f3917f950cb098c5979427c094822041569
Chantal <chantal.pilia01@gmail.com>
2022-04-12 01:42:28 -0500

    12.04 01:42

--- frontend/src/components/Shop/ShopComponent.jsx
+++ frontend/src/components/Shop/ShopComponent.jsx
@@ -105,48 +105,6 @@ function ShopComponent(props) {
     !loaded && fetchPlants();
   }, [])
 
-  // const dataClassSizeMedium = data.filter(item => item.size === "medium")
-  // // console.log(dataClassSizeMedium)
-
-  // const dataClassSizeSmall = data.filter(item => item.size === "small")
-  // // console.log(dataClassSizeSmall)
-
-  // const dataClassSizeLarge = data.filter(item => item.size === "large")
-  // // console.log(dataClassSizeLarge)
-
-  // const dataClassLowCare = data.filter(item => item.care === "Low Care")
-  // // console.log(dataClassLowCare)
-
-  // const dataClassMediumCare = data.filter(item => item.care === "Medium Care")
-  // // console.log(dataClassMediumCare)
-
-  // const dataClassHighCare = data.filter(item => item.care === "High Care")
-  // // console.log(dataClassHighCare)
-
-  // const dataClassLowLight = data.filter(item => item.light === "Low light")
-  // // console.log(dataClassLowLight)
-
-  // const dataClassMediumLight = data.filter(item => item.light === "Medium light")
-  // // console.log(dataClassMediumLight)
-
-  // const dataClassBrightLight = data.filter(item => item.light === "Bright light")
-  // // console.log(dataClassBrightLight)
-
-  // const dataClassByRoomBathroom = data.filter(item => item.room === "bathroom")
-  // // console.log(dataClassByRoomBathroom)
-
-  // const dataClassByRoomGarden = data.filter(item => item.room === "garden")
-  // // console.log(dataClassByRoomGarden)
-
-  // const dataClassByRoomBedroom = data.filter(item => item.room === "bedroom")
-  // // console.log(dataClassByRoomBedroom)
-
-  // const dataClassByRoomKitchen = data.filter(item => item.room === "kitchen")
-  // // console.log(dataClassByRoomKitchen)
-
-  // const dataClassByRoomLivingroom = data.filter(item => item.room === "living room")
-  // // console.log(dataClassByRoomLivingroom)
-
   return (
     <div className="wrapperShop">
 
